cd0a6e420a867e112497741c7cb164ea342f4c6c
Munizasaeed <munizasaeed85@gmail.com>
2026-09-25 17:46:42 +0500

    Added new feature X

--- script.js
+++ script.js
@@ -29,3 +29,24 @@ document.addEventListener('DOMContentLoaded', () => {
     loadNavbar();
     loadFooter();
 });
+
+let filterbutton = document.querySelectorAll('.btns .btn2');
+const foodCards = document.querySelectorAll('.cards-container .card');
+
+
+filterbutton.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        let selectedCategory = e.target.dataset.category;
+        console.log(selectedCategory);
+
+        foodCards.forEach((card) => { // Here parameter is 'card'
+            let cardCategory = card.dataset.category;
+
+            if (selectedCategory === 'all' || selectedCategory === cardCategory) {
+                card.style.display = 'block';
+            } else { // Added 'else' here
+                card.style.display = 'none';
+            }
+        });
+    });
+});
